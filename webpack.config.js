b17731dd521e60fa2561fb2da7537456bff219b2
maks <maksimrespekt.saper@gmail.com>
2025-02-21 02:55:56 +0500

    added sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,42 @@
 const path = require('path');
+const miniCss = require("mini-css-extract-plugin")
 
 module.exports = {
+  mode: "development",
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
+    assetModuleFilename: 'assets/images/[name][ext]'
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, 'public'),
     },
     compress: true,
     port: 8080,
-},
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          miniCss.loader,
+          'css-loader',
+          'sass-loader',
+        ]
+      }
+    ] 
+  },
+  plugins: [
+    new miniCss({
+      filename: 'style.css'
+    }),
+  ]
+
 };
