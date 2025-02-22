8c3d311bed4a643f370f1aaa67a982037e61509a
maks <maksimrespekt.saper@gmail.com>
2025-02-23 01:02:51 +0500

    add header

--- src/index.js
+++ src/index.js
@@ -12,3 +12,15 @@ import "./styles/main.scss"
 //   }
   
 //   document.body.appendChild(component());
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination]
+});
