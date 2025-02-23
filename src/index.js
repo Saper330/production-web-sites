78012b92c26755bf5e5a7a234b66bb5f649ee90b
maks <maksimrespekt.saper@gmail.com>
2025-02-23 20:58:28 +0500

    add section

--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
 // import _ from 'lodash';
 import "./styles/main.scss"
+import "./js/swiper.js"
 
-// function component() {
-//     const element = document.createElement('div');
-  
-//     // Lodash, currently included via a script, is required for this line to work
-//     // Lodash, now imported by this script
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-//     return element;
-//   }
-  
-//   document.body.appendChild(component());
-// core version + navigation, pagination modules:
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-// import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 
-// init Swiper:
-const swiper = new Swiper('.swiper', {
-  modules: [Navigation, Pagination]
-});
---
+++ src/js/swiper.js
@@ -0,0 +1,15 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  navigation: {
+    nextEl: '.swiper-button-prev',
+    prevEl: '.swiper-button-next'
+  },
+});
